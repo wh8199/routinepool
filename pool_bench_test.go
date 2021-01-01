453cd1efa08460f234ff5f0e86cd1c41c8d672fd
wh8199 <819971938@qq.com>
2021-01-01 10:29:05 +0800

    Add more benchmark functions

---
+++ pool_bench_test.go
@@ -0,0 +1,70 @@
+package routinepool
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkCreatingWorker(b *testing.B) {
+	config := DefaultRouterPoolConfig()
+	pool := NewRoutinePool(config)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		pool.getReadyWorkerWithoutPool()
+	}
+
+	b.StopTimer()
+}
+
+func BenchmarkCreatingWorkerWithPool(b *testing.B) {
+	config := DefaultRouterPoolConfig()
+	pool := NewRoutinePool(config)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		w := pool.getReadyWorker()
+		pool.workerPool.Put(w)
+	}
+
+	b.StopTimer()
+}
+
+func BenchmarkNoPool(b *testing.B) {
+	var wg sync.WaitGroup
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func() {
+			demoFunc()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	b.StopTimer()
+}
+
+func BenchmarkPool(b *testing.B) {
+	config := DefaultRouterPoolConfig()
+	config.WithCleanWorkerInterval("60s")
+	config.WithMaxIdleTime("120s")
+	pool := NewRoutinePool(config)
+	b.ResetTimer()
+	var wg sync.WaitGroup
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+
+		pool.SubmitWorker(func() {
+			demoFunc()
+			wg.Done()
+		})
+	}
+
+	wg.Wait()
+	b.StopTimer()
+}
--- pool_test.go
+++ pool_test.go
@@ -1,28 +1,78 @@
 package routinepool
 
 import (
+	"runtime"
+	"sync"
 	"testing"
+	"time"
 )
 
-func BenchmarkCreatingWorker(b *testing.B) {
-	config := DefaultRouterPoolConfig()
-	pool := NewRoutinePool(config)
+const (
+	RunTimes   = 1000000
+	BenchParam = 10
+)
 
-	b.ResetTimer()
+var curMem uint64
 
-	for i := 0; i < b.N; i++ {
-		pool.getReadyWorkerWithoutPool()
-	}
+const (
+	_   = 1 << (10 * iota)
+	KiB // 1024
+	MiB // 1048576
+	// GiB // 1073741824
+	// TiB // 1099511627776             (超过了int32的范围)
+	// PiB // 1125899906842624
+	// EiB // 1152921504606846976
+	// ZiB // 1180591620717411303424    (超过了int64的范围)
+	// YiB // 1208925819614629174706176
+)
+
+const (
+	Param    = 100
+	AntsSize = 1000
+	TestSize = 10000
+	n        = 10000000
+)
+
+func demoFunc() {
+	time.Sleep(time.Duration(BenchParam) * time.Millisecond)
 }
 
-func BenchmarkCreatingWorkerWithPool(b *testing.B) {
+func TestNoPool(t *testing.T) {
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			demoFunc()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	mem := runtime.MemStats{}
+	runtime.ReadMemStats(&mem)
+	curMem = mem.TotalAlloc/MiB - curMem
+	t.Logf("memory usage:%d MB", curMem)
+}
+
+func TestPool(t *testing.T) {
 	config := DefaultRouterPoolConfig()
+	config.WithCleanWorkerInterval("60s")
+	config.WithMaxIdleTime("120s")
 	pool := NewRoutinePool(config)
 
-	b.ResetTimer()
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
 
-	for i := 0; i < b.N; i++ {
-		w := pool.getReadyWorker()
-		pool.workerPool.Put(w)
+		pool.SubmitWorker(func() {
+			demoFunc()
+			wg.Done()
+		})
 	}
+
+	wg.Wait()
+	mem := runtime.MemStats{}
+	runtime.ReadMemStats(&mem)
+	curMem = mem.TotalAlloc/MiB - curMem
+	t.Logf("memory usage:%d MB", curMem)
 }
